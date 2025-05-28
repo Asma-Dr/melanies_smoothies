6db05a1526a1ca3c35ac33513ae42a33a698d17a
Asma-Dr <Asma.Draouil@esprit.tn>
2025-05-28 10:24:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ if ingredients_list :
         fruityvice_responce = requests.get("https://my.smoothiefroot.com/api/fruit/" +search_on)
         fv_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)
-        sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)
+        #sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
       
     
     #st.write(ingredients_string)
